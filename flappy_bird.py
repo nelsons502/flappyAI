ba4975d57ff0c49dbced390a037771220bd5bd90
Nelson Schnepf <nmschnepf@gmail.com>
2025-02-19 17:53:11 -0700

    working version of basic flappy bird

--- flappy_bird.py
+++ flappy_bird.py
@@ -5,11 +5,15 @@ import random
 # Initialize Pygame
 pygame.init()
 
+background = pygame.image.load("images/background.png")
+
 # Constants
-WIDTH, HEIGHT = 576, 1024
+WIDTH, HEIGHT = background.get_size()
 FPS = 60
 GRAVITY = 0.25
-FLAP_PWR = -5
+FLAP_PWR = -6 
+PIPE_GAP = 150
+score = 0
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -23,10 +27,8 @@ down = pygame.image.load("images/bird2.png")
 dead = pygame.image.load("images/birddead.png")
 fly_images = [up, flat, down, flat]
 
-top_pipe = pygame.image.load("images/top.png")
-bottom_pipe = pygame.image.load("images/bottom.png")
-
-background = pygame.image.load("images/background.png")
+top_img = pygame.image.load("images/top.png")
+bottom_img = pygame.image.load("images/bottom.png")
 
 
 # Bird class as an Actor/Sprite
@@ -41,7 +43,9 @@ class Bird(pygame.sprite.Sprite):
     def update(self):
         self.speed += GRAVITY
         self.rect.centery += self.speed
-        self.image = fly_images[(self.speed // 10) % 4]
+        self.image = fly_images[int(self.speed // 10) % 4]
+        # change angle of bird based on speed
+        self.image = pygame.transform.rotozoom(self.image, -self.speed * 3, 1)
     
     def flap(self):
         self.speed = FLAP_PWR
@@ -52,14 +56,94 @@ class Pipe(pygame.sprite.Sprite):
         super().__init__()
         self.image = image
         self.rect = self.image.get_rect()
-        self.rect.midbottom = pos
+        if image == top_img:
+            self.rect.midbottom = pos
+        else:
+            pos = (WIDTH, pos[1] + PIPE_GAP)
+            self.rect.midtop = pos
         self.speed = 3
+        self.scored = False
     
     def update(self):
         self.rect.centerx -= self.speed
-        if self.rect.right < 0:
+        if self.rect.right < 0: # remove pipe from the game
             self.kill()
         
     def collide(self, bird):
         return self.rect.colliderect(bird.rect)
     
+    def score(self, bird):
+        if self.rect.right < bird.rect.left:
+            if not self.scored:
+                self.scored = True
+                return True
+        return False
+
+    
+all_sprites = pygame.sprite.Group()
+pipes = pygame.sprite.Group()
+
+flappy = Bird()
+all_sprites.add(flappy)
+
+def spawn_pipes():
+    pipe_pos = random.randint(50, HEIGHT - 200)
+    top = Pipe(top_img, (WIDTH, pipe_pos))
+    bottom = Pipe(bottom_img, (WIDTH, pipe_pos))
+    all_sprites.add(top, bottom)
+    pipes.add(top, bottom)
+
+def draw_score():
+    font = pygame.font.Font(None, 50 )
+    text = font.render(str(score//2), True, (0,0,0))
+    screen.blit(text, (WIDTH // 2, 50))
+
+game_over = False
+pipe_timer = 0
+
+while True:
+    screen.blit(background, (0, 0))
+    draw_score()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if not game_over and event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                flappy.flap()
+    
+    all_sprites.update()
+    
+    pipe_timer += 1
+    if pipe_timer == 100 and not game_over:
+        pipe_timer = 0
+        spawn_pipes()
+    
+    for pipe in pipes:
+        if not game_over and pipe.collide(flappy) or flappy.rect.top < 0 or flappy.rect.bottom > HEIGHT:
+            flappy.image = dead
+            flappy.speed = 0
+            pipes.empty()
+            game_over = True
+            pipe_timer = 0
+            break
+        if not game_over and pipe.score(flappy):
+            score += 1
+    
+    all_sprites.draw(screen)
+
+    if game_over:
+        font = pygame.font.Font(None, 100)
+        text = font.render("Game Over", True, (200,0,0))
+        screen.blit(text, (WIDTH // 4, HEIGHT // 2))
+        if pipe_timer == 100:
+            break
+
+    
+    
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
+    
+
